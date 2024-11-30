6b37fae4df5e17eb9d4d175962b530c339ba6d72
Shean <S.sarawutis07@gmail.com>
2024-11-30 12:30:11 +0000

    เพิ่มโปรเจคเข้าไป แก้บัคลิงก์เชื่อมโยงกัน

--- Script.js
+++ Script.js
@@ -1,3 +1,7 @@
+function toggleLargeImage(imageId) {
+  var image = document.getElementById(imageId);
+  // You can add any image enlargement or handling here if needed
+}
 
 document.getElementById('modalImage').src = imgSrc;
   document.getElementById('modalTitle').innerText = title;
